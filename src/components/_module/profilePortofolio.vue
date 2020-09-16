c332f39f720f5d22725661e1e379b6d8c650a625
Glitchfer <arif.0496.rahman@gmail.com>
2020-09-16 22:34:05 +0700

    Update compoenen profile portofolio card

---
+++ src/components/_module/profilePortofolio.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Main />
+  </div>
+</template>
+
+<script>
+import Main from '../_base/profileCard'
+export default {
+  name: 'Portofolio',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+  margin: 0 auto;
+  padding-top: 50px;
+  width: 100%;
+}
+</style>
