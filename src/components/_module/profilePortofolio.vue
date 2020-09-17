f93441dcc201341f108e265e2c65a40c95299650
Glitchfer <arif.0496.rahman@gmail.com>
2020-09-17 09:07:51 +0700

    Update footer profile page

--- src/components/_module/profilePortofolio.vue
+++ src/components/_module/profilePortofolio.vue
@@ -1,19 +1,28 @@
 <template>
-  <b-container fluid class="container">
-    <div class="headerBg"></div>
-    <Cardprofile />
-    <Protofoliotab />
-  </b-container>
+  <div>
+    <b-container fluid class="container">
+      <div class="headerBg"></div>
+      <main>
+        <Cardprofile />
+        <Protofoliotab />
+      </main>
+    </b-container>
+    <footer>
+      <Footer />
+    </footer>
+  </div>
 </template>
 
 <script>
 import Cardprofile from '../_base/profileCard'
 import Protofoliotab from '../_base/portofolioTab'
+import Footer from '../_base/footer'
 export default {
   name: 'Portofolio',
   components: {
     Cardprofile,
-    Protofoliotab
+    Protofoliotab,
+    Footer
   }
 }
 </script>
@@ -24,9 +33,19 @@ export default {
   margin: 0 auto;
   padding-top: 50px;
   width: 100%;
+  /* display: flex;
+  flex-wrap: nowrap;
+  box-sizing: border-box; */
+}
+main {
+  text-align: center;
+  margin: 0 auto;
+  padding-top: 50px;
+  width: 100%;
   display: flex;
   flex-wrap: nowrap;
   box-sizing: border-box;
+  position: relative;
 }
 .headerBg {
   background-color: #5e50a1;
@@ -34,6 +53,14 @@ export default {
   height: 300px;
   position: absolute;
   top: 0;
+  left: 0;
+}
+footer {
+  background-color: #5e50a1;
+  width: 100%;
+  height: 300px;
+  position: relative;
+  bottom: 0;
   left: 0;
 }
 </style>
